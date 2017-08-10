c6f1294fd6bb76057c91136d3cf26bfb4bd5c90f
Christof Kälin <christof.kaelin@gmail.com>
2017-08-10 20:18:16 +0200

    test updates

--- src/App.test.js
+++ src/App.test.js
@@ -5,9 +5,15 @@ import { shallow } from 'enzyme';
 import App from './App';
 
 it('renders shallow', () => {
-    shallow(<App />);
+    const wrapper = shallow(<App />);
+    const welcome = <h2>Our daily bread</h2>;
+    expect(wrapper.contains(welcome)).toEqual(true);
+    expect(wrapper).toContainReact(welcome);
+    return false;
 });
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
 });
+
+
