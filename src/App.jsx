1006fe7de00286b1dd0346e1ebc2278d7e9a3520
Chrigi <ckpinguin@circleci.com>
2017-09-02 17:20:36 +0200

    formatting

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ export default class App extends Component {
             'dayMinus':   ['down', 'left']
         };
         const handlers = {
-            'dayPlus':     () => this.updateDate(getDateWithChangedDays(this.state.date, +1)),
+            'dayPlus':    () => this.updateDate(getDateWithChangedDays(this.state.date, +1)),
             'dayMinus':   () => this.updateDate(getDateWithChangedDays(this.state.date, -1))
         };
         const url = this.getImgUrl();
