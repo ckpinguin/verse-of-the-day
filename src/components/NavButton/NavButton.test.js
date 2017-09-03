de1c1336928cf742ae3c46fe6c2cf1e12cd6e74f
Chrigi <ckpinguin@circleci.com>
2017-09-03 20:57:08 +0200

    more test setups

--- src/components/NavButton/NavButton.test.js
+++ src/components/NavButton/NavButton.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactTestUtils from 'react-dom/test-utils';
-import { shallow, mount } from 'enzyme'; 
+// import ReactTestUtils from 'react-dom/test-utils';
+import { mount } from 'enzyme'; 
 // import renderer from 'react-test-renderer';
 
 import NavButton from '.';
@@ -11,27 +11,33 @@ describe('<NavButton />', () => {
     let props;
     let mountedNavButton;
     const onClick=() => {};
+    const navButtonJSX = <NavButton {...props} />;
     const navButton = () => {
         if (!mountedNavButton) {
             mountedNavButton = mount(
-                <NavButton
-                    onClick={onClick}
-                />
+                navButtonJSX
             );
         }
         return mountedNavButton;
-    }
+    };
+    
+    beforeEach(() => {
+        props = {
+            onClick: onClick,
+            icon: 'fa fa-arrow-up'
+        };
+        mountedNavButton = undefined;
+    });
+
     it('renders without crashing', () => {
         const div = document.createElement('div');
         ReactDOM.render(
-            <NavButton
-                onClick={() => {}}
-            />, div);
+            navButtonJSX, div);
     });
 
     /** Just an example, normally props should not be tested, as they
         are already covered by PropTypes. */
-    it('allows us to set props', () => {
+/*     it('allows us to set props', () => {
         const onClick = () => console.log('onClick() fired');
         const wrapper = mount(
             <NavButton
@@ -41,16 +47,18 @@ describe('<NavButton />', () => {
         expect(wrapper.props().propOne).toEqual('foo');
         wrapper.setProps({ propOne: 'bonka' });
         expect(wrapper.props().propOne).toEqual('bonka');
+    }); */
+
+    it('mounts NavButton on FullDOM rendering', () => {
+        const renderedTag = navButton().find('NavButton');
+        expect(renderedTag.length).toBe(1);
     });
 
     it('handles click events', () => {
         NavButton.prototype.onClick = jest.fn();    
         const mockClick = NavButton.prototype.onClick;
         const wrapper = mount((
-            <NavButton
-                onClick={mockClick}
-                icon="fa fa-arrow-up"
-            />
+            navButtonJSX
         ));
         wrapper.find('button').simulate('click');
         expect(mockClick).toHaveBeenCalled();
